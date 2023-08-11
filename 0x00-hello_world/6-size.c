1692b7a69e65b9bc0583f251e34ea3a7dc4b2957
Mintahsam <mintahsam2777@gmail.com>
2023-08-10 19:36:14 -0700

    Hello World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Sizes of a char: %d bute(s)\n", sizeof(char));
+	printf("Sizes of a int: %d bute(s)\n", sizeof(int));
+	printf("Sizes of a long int: %d bute(s)\n", sizeof(long int));
+	printf("Sizes of a long long int: %d bute(s)\n", sizeof(long long int));
+	printf("Sizes of a float: %d bute(s)\n", sizeof(float));
+	return (0);
+}
